8e380b6c94bf229028281b6b5087ac079505c270
McEarl <ms@marcelschuetz.de>
2020-06-23 18:35:28 +0200

    Add spec

---
+++ spec/language-ftl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageFtlView from '../lib/language-ftl-view';
+
+describe('LanguageFtlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
