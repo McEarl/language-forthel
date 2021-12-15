94e46c473ddd63f5e632678b213a010c8fc6682a
McEarl <ms@marcelschuetz.de>
2021-12-15 16:22:47 +0100

    Remove spec files

--- spec/language-ftl-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageFtlView from '../lib/language-ftl-view';
-
-describe('LanguageFtlView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
